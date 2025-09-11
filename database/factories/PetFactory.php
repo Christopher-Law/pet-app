4ec9a5905f24f6076b8d00adf065b230027f8b64
Christopher-Law <christopher-law@live.com>
2025-09-11 14:03:53 -0400

    update factory to use the breed enum

--- database/factories/PetFactory.php
+++ database/factories/PetFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\Breed;
 use App\Enums\Sex;
 use App\Enums\Type;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -21,7 +22,7 @@ class PetFactory extends Factory
         return [
             'name' => fake()->name(),
             'type' => fake()->randomElement(Type::cases()),
-            'breed' => fake()->randomElement(['Labrador', 'German Shepherd', 'Golden Retriever', 'Bulldog', 'Poodle', 'Beagle', 'Dachshund', 'Shih Tzu', 'Pug', 'Chihuahua', 'Other']),
+            'breed' => fake()->randomElement(Breed::cases()),
             'date_of_birth' => fake()->date(),
             'sex' => fake()->randomElement(Sex::cases()),
             'is_dangerous_animal' => fake()->boolean(),
